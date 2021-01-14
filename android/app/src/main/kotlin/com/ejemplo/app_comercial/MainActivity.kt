ba3c3fad250cb4b34b4cf250d99888651fe85a7c
Soulball <navarrete.de16@tectijuana.edu.mx>
2021-01-14 14:39:58 -0800

    Initial project

---
+++ android/app/src/main/kotlin/com/ejemplo/app_comercial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ejemplo.app_comercial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App comercial informativa',
+      home: Scaffold()
+    );
+  }
+}
+
+
